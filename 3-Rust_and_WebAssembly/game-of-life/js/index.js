d6b106e491ebaff298f130e575bc484eb5ec3d96
Prince <this.is.kumar.prince@gmail.com>
2022-01-19 19:32:18 +0530

    game of life implemented

--- 3-Rust_and_WebAssembly/game-of-life/js/index.js
+++ 3-Rust_and_WebAssembly/game-of-life/js/index.js
@@ -1,5 +1,14 @@
 import("../pkg/index.js")
   .then((module) => {
-    module.start();
+    const { Universe } = module;
+    const pre = document.getElementById("game-of-life-canvas");
+    const universe = Universe.new();
+    const renderLoop = () => {
+      pre.textContent = universe.render();
+      universe.tick();
+
+      requestAnimationFrame(renderLoop);
+    };
+    requestAnimationFrame(renderLoop);
   })
   .catch(console.error);
