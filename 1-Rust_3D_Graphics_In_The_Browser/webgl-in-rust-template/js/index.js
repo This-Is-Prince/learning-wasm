da7f97e510d5d654c135d051654862f449ee9cd0
Prince <this.is.kumar.prince@gmail.com>
2022-01-20 13:55:44 +0530

    template ready

--- 1-Rust_3D_Graphics_In_The_Browser/webgl-in-rust-template/js/index.js
+++ 1-Rust_3D_Graphics_In_The_Browser/webgl-in-rust-template/js/index.js
@@ -1,5 +1,51 @@
-import("../pkg/index.js")
-  .then((module) => {
-    module.start();
+const rust = import("../pkg/index.js");
+const canvas = document.getElementById("rustCanvas");
+const gl = canvas.getContext("webgl", { antialias: true });
+
+rust
+  .then((m) => {
+    if (!gl) {
+      alert("Failed to initialize WebGL");
+      return;
+    }
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+    const FPS_THROTTLE = 1000.0 / 30.0; // milliseconds / frames
+    const princeClient = new m.PrinceClient();
+    const initialTime = Date.now();
+    let lastDrawTime = -1; // In milliseconds
+
+    function render() {
+      window.requestAnimationFrame(render);
+      const currTime = Date.now();
+
+      if (currTime >= lastDrawTime + FPS_THROTTLE) {
+        lastDrawTime = currTime;
+        const { innerHeight, innerWidth } = window;
+
+        if (
+          window.innerHeight != canvas.height ||
+          window.innerWidth != canvas.width
+        ) {
+          canvas.height = innerHeight;
+          canvas.clientHeight = innerHeight;
+          canvas.style.height = innerHeight;
+
+          canvas.width = innerWidth;
+          canvas.clientWidth = innerWidth;
+          canvas.style.width = innerWidth;
+
+          gl.viewport(0, 0, innerWidth, innerHeight);
+        }
+        let elapsedTime = currTime - initialTime;
+        princeClient.update(elapsedTime, innerHeight, innerWidth);
+        princeClient.render();
+        // Rust Update call
+        // Rust Render call
+      }
+    }
+    render();
   })
   .catch(console.error);
--- 1-Rust_3D_Graphics_In_The_Browser/webgl-in-rust-template/src/lib.rs
+++ 1-Rust_3D_Graphics_In_The_Browser/webgl-in-rust-template/src/lib.rs
@@ -1,21 +1,35 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
+/*
+Wee Allocator
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-// This is like the `main` function, except for JavaScript.
-#[wasm_bindgen(start)]
-pub fn run() {
-    console::log_1(&JsValue::from_str("running...."));
-}
+*/
 
 #[wasm_bindgen]
 pub fn start() {
     console::log_1(&JsValue::from_str("start...."));
+}
+
+#[wasm_bindgen]
+pub struct PrinceClient {}
+
+#[wasm_bindgen]
+impl PrinceClient {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        console::log_1(&JsValue::from_str("princeClient...."));
+
+        Self {}
+    }
+
+    pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+        // console::log_1(&JsValue::from_str("update...."));
+        Ok(())
+    }
+    pub fn render(&self) {
+        // console::log_1(&JsValue::from_str("render...."));
+    }
 }
