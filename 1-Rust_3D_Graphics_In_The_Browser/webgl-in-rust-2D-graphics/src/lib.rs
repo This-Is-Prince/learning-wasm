d581774a752b134a3e8c475ab75921e3ca0fcd99
Prince <this.is.kumar.prince@gmail.com>
2022-01-20 15:33:29 +0530

    single triangle render

--- 1-Rust_3D_Graphics_In_The_Browser/webgl-in-rust-2D-graphics/src/lib.rs
+++ 1-Rust_3D_Graphics_In_The_Browser/webgl-in-rust-2D-graphics/src/lib.rs
@@ -1,5 +1,11 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
+use web_sys::WebGlRenderingContext as GL;
+use web_sys::*;
+
+mod common_funcs;
+mod gl_setup;
+mod programs;
+mod shaders;
 
 /*
 Wee Allocator
@@ -14,15 +20,21 @@ pub fn start() {
 }
 
 #[wasm_bindgen]
-pub struct PrinceClient {}
+pub struct PrinceClient {
+    gl: WebGlRenderingContext,
+    program_color_2d: programs::Color2D,
+}
 
 #[wasm_bindgen]
 impl PrinceClient {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
-        console::log_1(&JsValue::from_str("princeClient...."));
-
-        Self {}
+        console_error_panic_hook::set_once();
+        let gl = gl_setup::initialize_webgl_context().unwrap();
+        Self {
+            program_color_2d: programs::Color2D::new(&gl),
+            gl,
+        }
     }
 
     pub fn update(&mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
@@ -30,6 +42,15 @@ impl PrinceClient {
         Ok(())
     }
     pub fn render(&self) {
-        // console::log_1(&JsValue::from_str("render...."));
+        self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+        self.program_color_2d.render(
+            &self.gl, //webgl context
+            100.,     //bottom
+            200.,     //top
+            100.,     //left
+            200.,     //right
+            400.,     //canvas_height
+            400.,     //canvas_width
+        );
     }
 }
